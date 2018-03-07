ca15d3f6afb506417fea9eb14602a946e0a6c225
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2018-03-07 15:52:06 -0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject core "0.1.0-SNAPSHOT"
-  :description "The core Sov business logic in portable Clojure to run on the JVM, JS or .NET."
+  :description "The core Sov business logic."
   :url "https://github.com/sovteam/core"
   :license {:name "GNU Geberal Public License v3.0"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
