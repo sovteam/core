d374b21696f465f7011c6a5ea382865c5c47abde
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2018-03-21 18:17:42 -0300

    Minor

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject core "0.1.0-SNAPSHOT"
   :description "The core Sov business logic."
   :url "https://github.com/sovteam/core"
-  :license {:name "GNU Geberal Public License v3.0"
+  :license {:name "GNU General Public License v3.0"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]]
